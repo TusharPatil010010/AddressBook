072c8e2a8f9fd34c1e6838d603cf109478f9c344
TusharPatil010010 <tusharp010010@gmail.com>
2020-09-29 21:04:32 +0530

    Added the ability to create a new contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,50 @@
-public class AddressBook{
-    
-    public static void main(String[] args){
-        System.out.println("Welcome to address book program");
-    }
-    
+import java.util.Scanner;
+
+public class AddressBook {
+	public static void main(String[] args) {
+
+		//variables
+		String first_name;
+		String last_name;
+		String address;
+		String city;
+		String state;
+		int zip;
+		long  phone_num;
+		String email;
+		
+		System.out.println("Welcome to Address Book");
+
+		//Taking input from console
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter your First Name : ");
+		first_name = sc.nextLine();
+
+		System.out.println("Enter your Last Name : ");
+		last_name = sc.nextLine();
+
+		System.out.println("Enter your Address : ");
+		address = sc.nextLine();
+
+		System.out.println("City : ");
+		city = sc.nextLine();
+
+		System.out.println("State : ");
+		state = sc.nextLine();
+
+		System.out.println("ZIP : ");
+		zip = sc.nextInt();
+
+		System.out.println("Enter your Phone number : ");
+		phone_num = sc.nextLong();
+		sc.nextLine();
+
+		System.out.println("Enter your Email ID : ");
+		email = sc.nextLine();
+
+		//calling person class
+		person person1 = new person(first_name, last_name, address, city, state, zip, phone_num, email);
+		
+	}
 }
