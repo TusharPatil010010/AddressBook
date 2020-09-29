016ee384e7124dfc5bdfde0ce6aa08902691f240
TusharPatil010010 <tusharp010010@gmail.com>
2020-09-29 21:07:31 +0530

    Added ability to edit existing contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,50 +1,118 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBook {
-	public static void main(String[] args) {
 
-		//variables
-		String first_name;
-		String last_name;
+	public List <person> personList;
+	public AddressBook() {
+		personList = new ArrayList<person>();
+	}
+	
+	public void displayAllContacts() {
+		System.out.println("DISPLAYING ALL CONTACT DETAILS FROM ADDRESS BOOK");
+		for(int i = 0; i < personList.size(); i++) {
+			System.out.println(personList.get(i));
+		}
+	}
+	
+	
+	public void addPersonDetails() {
+		Scanner sc = new Scanner(System.in);
+		
+		String checkToAdd = "y";
+		
+		while(checkToAdd.equalsIgnoreCase("y")) {
+			//variables
+			String first_name;
+			String last_name;
+			String address;
+			String city;
+			String state;
+			int zip;
+			long  phone_num;
+			String email;
+			
+			System.out.println("Enter following details : ");
+			System.out.println("First Name : ");
+			first_name = sc.nextLine();
+			System.out.println("Last Name : ");
+			last_name = sc.nextLine();
+			System.out.println("Address : ");
+			address = sc.nextLine();
+			System.out.println("City : ");
+			city = sc.nextLine();
+			System.out.println("State : ");
+			state = sc.nextLine();
+			System.out.println("ZIP : ");
+			zip = sc.nextInt();
+			System.out.println("Phone number : ");
+			phone_num = sc.nextLong();
+			sc.nextLine();
+			System.out.println("Email ID : ");
+			email = sc.nextLine();
+			
+			person new_person = new person(first_name, last_name, address, city, state, zip, phone_num, email);
+			personList.add(new_person);
+			
+			System.out.println("Enter 'y' to add new person's details.\nEnter any other key to stop.");
+			checkToAdd = sc.nextLine();
+		}
+		
+	}
+	
+	public void editPersonDetails() {
+		String FirstName;
+		String LastName;
 		String address;
 		String city;
 		String state;
 		int zip;
 		long  phone_num;
 		String email;
-		
-		System.out.println("Welcome to Address Book");
-
-		//Taking input from console
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter your First Name : ");
-		first_name = sc.nextLine();
-
-		System.out.println("Enter your Last Name : ");
-		last_name = sc.nextLine();
-
-		System.out.println("Enter your Address : ");
-		address = sc.nextLine();
-
-		System.out.println("City : ");
-		city = sc.nextLine();
-
-		System.out.println("State : ");
-		state = sc.nextLine();
-
-		System.out.println("ZIP : ");
-		zip = sc.nextInt();
-
-		System.out.println("Enter your Phone number : ");
-		phone_num = sc.nextLong();
-		sc.nextLine();
-
-		System.out.println("Enter your Email ID : ");
-		email = sc.nextLine();
-
-		//calling person class
-		person person1 = new person(first_name, last_name, address, city, state, zip, phone_num, email);
+		
+		System.out.println("Enter name of a person to edit contact details");
+		System.out.println("First Name : ");
+		FirstName = sc.nextLine();
+		System.out.println("Last Name : ");
+		LastName = sc.nextLine();
+		
+		for(int i = 0; i < personList.size(); i++) {
+			if(FirstName.equalsIgnoreCase(personList.get(i).first_name) && LastName.equalsIgnoreCase(personList.get(i).last_name)) {
+				System.out.println("Address : ");
+				address = sc.nextLine();
+				System.out.println("City : ");
+				city = sc.nextLine();
+				System.out.println("State : ");
+				state = sc.nextLine();
+				System.out.println("ZIP : ");
+				zip = sc.nextInt();
+				System.out.println("Phone number : ");
+				phone_num = sc.nextLong();
+				sc.nextLine();
+				System.out.println("Email ID : ");
+				email = sc.nextLine();
+				
+				person new_person = new person(FirstName, LastName, address, city, state, zip, phone_num, email);
+				personList.set(i, new_person);
+			}
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO ADDRESS BOOK");
+		
+		AddressBook address_book_main = new AddressBook();
+		address_book_main.addPersonDetails();
+		
+		//Displaying all contacts from Address book
+		address_book_main.displayAllContacts();
+		
+		//editing details
+		address_book_main.editPersonDetails();
+		
+		//Displaying all contacts from Address book after editing
+		address_book_main.displayAllContacts();
 		
 	}
 }
--- person.java
+++ person.java
@@ -1,4 +1,4 @@
-//package AddressBookMain;
+package AddressBookMain;
 
 public class person {
 
@@ -11,8 +11,8 @@ public class person {
 	public long  phone_num;
 	public String email;
 	
-	public person(String first_name, String last_name, String address, String city, String state, int zip, long phone_num, String email) {	
-	
+	public person(String first_name, String last_name, String address, String city, String state, int zip, long phone_num, String email) {
+
 		this.first_name = first_name;
 		this.last_name = last_name;
 		this.address = address;
@@ -21,5 +21,12 @@ public class person {
 		this.zip = zip;
 		this.phone_num = phone_num;
 		this.email = email;
+	}
+	
+	public String toString() {
+
+		String details = "First Name : " + first_name + "\nLast Name : " + last_name + "\nAddress : " + address + "\nCity : " + city 
+							+ "\nState : " + state + "\nZIP : " + zip + "\nPhone Number : " + phone_num + "\nEmail ID : " + email + "\n";
+		return details;
 	}
 }
