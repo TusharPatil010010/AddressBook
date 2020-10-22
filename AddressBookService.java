41c232928e0def0a4729ece0612278dff86ddfbc
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-22 17:59:18 +0530

    Refactored the code to read and write CSV

--- AddressBookService.java
+++ AddressBookService.java
@@ -4,11 +4,12 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 public class AddressBookService {
-	public static String FILE_NAME = "AddressBook.text";
-	
-	//File IO operations
+	public static String FILE_NAME = "AddressBook.txt";
+
 	public void writeData(Map<String, AddressBook> addressBookMap) {
 		StringBuffer employeeBuffer = new StringBuffer();
 		for(Map.Entry<String, AddressBook> entry : addressBookMap.entrySet()) {
@@ -27,6 +28,55 @@ public class AddressBookService {
 		try {
 			Files.lines(new File(FILE_NAME).toPath()).forEach(System.out::println);
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void writeDataCSV(Map<String, AddressBook> stateAddressBookMap) {
+
+		File file = new File(
+				"C:\\Users\\abc\\eclipse-workspace\\addressbooksystem\\src\\main\\java\\com\\rohan\\addressbooksystem/addressbook.csv");
+
+		try {
+			FileWriter outputfile = new FileWriter(file);
+			CSVWriter writer = new CSVWriter(outputfile);
+			List<String[]> data = new ArrayList<String[]>();
+			String[] header = { "First Name", "Last Name", "Address", "City", "State", "ZIP", "Phone Number",
+					"Email ID" };
+			data.add(header);
+			stateAddressBookMap.values().stream().map(entry -> entry.getPersonList())
+					.forEach(entryt -> entryt.forEach(person -> {
+						String[] personData = { person.getFirstName(), person.getLastName(), person.getAddress(),
+								person.getCity(), person.getState(), Integer.toString(person.getZip()),
+								Long.toString(person.getPhoneNumber()), person.getEmail() };
+						data.add(personData);
+					}));
+
+			writer.writeAll(data);
+			writer.close();
+			System.out.println("Data entered successfully to addressbook.csv file.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void readDataCSV() {
+		try {
+			FileReader filereader = new FileReader(
+					"C:\\Users\\abc\\eclipse-workspace\\addressbooksystem\\src\\main\\java\\com\\rohan\\addressbooksystem/addressbook.csv");
+			CSVReader csvReader = new CSVReader(filereader);
+			String[] nextRecord;
+
+			// we are going to read data line by line
+			while ((nextRecord = csvReader.readNext()) != null) {
+				for (String cell : nextRecord) {
+					System.out.print(cell + "\t");
+				}
+				System.out.println();
+			}
+			csvReader.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
